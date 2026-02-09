ea408e1b0075b108b804e39d90c5af92c1d71d6a
David19656 <david.louboutin@stfelixlasalle.fr>
2026-02-09 16:58:10 +0100

    BDD, PHP
    - MAJ bdd.sql
    - connexion.inc.php
    - index.php

--- BDD/bdd.sql
+++ BDD/bdd.sql
@@ -1,59 +1,138 @@
--- ----------------------------------------------------------
--- Script MYSQL pour mcd 
--- ----------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 09 fév. 2026 à 15:56
+-- Version du serveur : 9.1.0
+-- Version de PHP : 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- ----------------------------
--- Table: user
--- ----------------------------
-CREATE TABLE user (
-  id_user INT NOT NULL,
-  id_LDAP VARCHAR(50) NOT NULL,
-  admin TINYINT(1) NOT NULL,
-  block TINYINT(1) NOT NULL,
-  nbr_connexion INT NOT NULL,
-  nbr_requete INT NOT NULL,
-  nbr_doc_sent INT NOT NULL,
-  CONSTRAINT user_PK PRIMARY KEY (id_user)
-)ENGINE=InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de données : `projet1_chatbot`
+--
 
--- ----------------------------
--- Table: session
--- ----------------------------
-CREATE TABLE session (
-  id_session INT NOT NULL,
-  name VARCHAR(50) NOT NULL,
-  last_updated DATETIME NOT NULL,
-  created_at DATETIME NOT NULL,
-  id_user INT NOT NULL,
-  CONSTRAINT session_PK PRIMARY KEY (id_session),
-  CONSTRAINT session_id_user_FK FOREIGN KEY (id_user) REFERENCES user (id_user)
-)ENGINE=InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `fichier`
+--
 
--- ----------------------------
--- Table: logs
--- ----------------------------
-CREATE TABLE logs (
-  id_logs INT NOT NULL,
-  date DATETIME NOT NULL,
-  ip_address VARCHAR(50) NOT NULL,
-  id_user INT NOT NULL,
-  CONSTRAINT logs_PK PRIMARY KEY (id_logs),
-  CONSTRAINT logs_id_user_FK FOREIGN KEY (id_user) REFERENCES user (id_user)
-)ENGINE=InnoDB;
+DROP TABLE IF EXISTS `fichier`;
+CREATE TABLE IF NOT EXISTS `fichier` (
+  `id_fichier` int NOT NULL,
+  `filename` varchar(100) NOT NULL,
+  `id_session` int NOT NULL,
+  PRIMARY KEY (`id_fichier`),
+  KEY `fichier_id_session_FK` (`id_session`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table: message
--- ----------------------------
-CREATE TABLE message (
-  id_message INT NOT NULL,
-  content VARCHAR(4000) NOT NULL,
-  date DATETIME NOT NULL,
-  sender VARCHAR(50) NOT NULL,
-  id_session INT NOT NULL,
-  CONSTRAINT message_PK PRIMARY KEY (id_message),
-  CONSTRAINT message_id_session_FK FOREIGN KEY (id_session) REFERENCES session (id_session)
-)ENGINE=InnoDB;
+--
+-- Structure de la table `logs`
+--
+
+DROP TABLE IF EXISTS `logs`;
+CREATE TABLE IF NOT EXISTS `logs` (
+  `id_logs` int NOT NULL,
+  `date` datetime NOT NULL,
+  `ip_address` varchar(50) NOT NULL,
+  `id_user` int NOT NULL,
+  PRIMARY KEY (`id_logs`),
+  KEY `logs_id_user_FK` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE IF NOT EXISTS `message` (
+  `id_message` int NOT NULL,
+  `content` varchar(4000) NOT NULL,
+  `date` datetime NOT NULL,
+  `sender` varchar(50) NOT NULL,
+  `id_session` int NOT NULL,
+  PRIMARY KEY (`id_message`),
+  KEY `message_id_session_FK` (`id_session`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `session`
+--
+
+DROP TABLE IF EXISTS `session`;
+CREATE TABLE IF NOT EXISTS `session` (
+  `id_session` int NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `last_updated` datetime NOT NULL,
+  `created_at` datetime NOT NULL,
+  `id_user` int NOT NULL,
+  PRIMARY KEY (`id_session`),
+  KEY `session_id_user_FK` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` int NOT NULL,
+  `id_LDAP` varchar(50) NOT NULL,
+  `admin` tinyint(1) NOT NULL,
+  `block` tinyint(1) NOT NULL,
+  `nbr_connexion` int NOT NULL,
+  `nbr_requete` int NOT NULL,
+  `nbr_doc_sent` int NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `fichier`
+--
+ALTER TABLE `fichier`
+  ADD CONSTRAINT `fichier_id_session_FK` FOREIGN KEY (`id_session`) REFERENCES `session` (`id_session`);
+
+--
+-- Contraintes pour la table `logs`
+--
+ALTER TABLE `logs`
+  ADD CONSTRAINT `logs_id_user_FK` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Contraintes pour la table `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `message_id_session_FK` FOREIGN KEY (`id_session`) REFERENCES `session` (`id_session`);
+
+--
+-- Contraintes pour la table `session`
+--
+ALTER TABLE `session`
+  ADD CONSTRAINT `session_id_user_FK` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
