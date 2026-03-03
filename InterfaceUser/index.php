14ecf45aed0266ebff4a1097b19f8a8536f6ac61
Bee <femy.domingo@stfelixlasalle.fr>
2026-03-03 16:10:49 +0100

    Sidebar - Initialisation
    - Ajout de la page sidebar
    - Ajout des liens aux autres pages
    - css de la navbar

--- InterfaceUser/index.php
+++ InterfaceUser/index.php
@@ -1,33 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./variables.css">
-    <link rel="stylesheet" href="css/style.css">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./variables.css" />
+    <link rel="stylesheet" href="css/style.css" />
     <title>Chatbot</title>
-</head>
-<body>
-    <div class="lateral">
-        <div class="titleLateral">
-            <h3>TITLE</h3>
+  </head>
+  <body>
+    <div class="sidebar">
+        <div class="headerSidebar">
+            <h3 class="titleSidebar">ChatBot</h3>
+            <button class="buttonSidebar">-</button>
         </div>
-        <ul class="listLateral">
-            <div class="listFixLateral">
-                <li>Nouveau chat  +</li>
-                <li>Historique</li>    
+        <ul class="listSidebar">
+            <div class="listFixSidebar">
+                <li><a href="newchat.php">Nouveau chat  +</a></li>
+                <li><a href="history.php">Historique</a></li>    
             </div>
-            <hr>
-            <div class="listChatLateral">
-                <li>Chat</li>
-                <li>Chat</li>
-                <li>Chat</li>
+            <hr class="hrSidebar">
+            <div class="listChatSidebar">
+                <li><a href="history.php">Chat</a></li>  
+                <li><a href="history.php">Chat</a></li>  
+                <li><a href="history.php">Chat</a></li> 
             </div>
             
         </ul>
     </div>
     <div class="mainChat">
-            <h1>TEST</h1>
+        <h1>TEST</h1>
     </div>
-</body>
+  </body>
 </html>
---
+++ InterfaceUser/sidebar.php
@@ -0,0 +1,18 @@
+<div class="sidebar">
+    <div class="headerSidebar">
+        <h3 class="titleSidebar">ChatBot</h3>
+        <button class="buttonSidebar">-</button>
+    </div>
+    <ul class="listSidebar">
+        <div class="listFixSidebar">
+            <li><a href="newchat.php">Nouveau chat  +</a></li>
+            <li><a href="history.php">Historique</a></li>    
+        </div>
+        <hr class="hrSidebar">
+        <div class="listChatSidebar">
+            <li><a href="history.php">Chat</a></li>  
+            <li><a href="history.php">Chat</a></li>  
+            <li><a href="history.php">Chat</a></li> 
+        </div>    
+    </ul>
+</div>
