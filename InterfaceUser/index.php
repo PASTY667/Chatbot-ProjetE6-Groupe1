e7c0c24367c94d3781834afeeb303ea9f52aee9c
Bee <femy.domingo@stfelixlasalle.fr>
2026-03-03 11:13:40 +0100

    Variables et css
    - Fichier variable (nommage, couleurs principales...)
    - Début de la structure html
    - Début de l'incorporation du css

--- InterfaceUser/index.php
+++ InterfaceUser/index.php
@@ -3,9 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./variables.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Chatbot</title>
 </head>
 <body>
-    
+    <div class="lateral">
+        <div class="titleLateral">
+            <h3>TITLE</h3>
+        </div>
+        <ul class="listLateral">
+            <div class="listFixLateral">
+                <li>Nouveau chat  +</li>
+                <li>Historique</li>    
+            </div>
+            <hr>
+            <div class="listChatLateral">
+                <li>Chat</li>
+                <li>Chat</li>
+                <li>Chat</li>
+            </div>
+            
+        </ul>
+    </div>
+    <div class="mainChat">
+            <h1>TEST</h1>
+    </div>
 </body>
 </html>
