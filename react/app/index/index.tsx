2a1e6584fe5711ad30876523605eda909a33886b
Bee <femy.domingo@stfelixlasalle.fr>
2026-03-24 09:58:11 +0100

    Installation MUI
    - Installation MUI
    - Le reste je sais plus, it feels like yesterday

--- react/app/index/index.tsx
+++ react/app/index/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { TextField, Button, Container, Grid, LinearProgress, CircularProgress } from "@mui/material";
 import style from "@/app/index/index.module.css";
 import uploadIcon from "@/assets/upload-file.png";
 import logo from "@/assets/frankLogo.png";
@@ -6,20 +7,41 @@ import logo from "@/assets/frankLogo.png";
 export default function Index() {
   return (
     <div className={style.newchatPage}>
+      {/* HEADER DE LA PAGE */}
       <div className={style.newchatHeader}>
         {/* <Image className={style.logo} src={} alt="" /> */}
-      <h1 className={style.newchatTitle}>Franklin</h1>
-    </div>
-    <div className={style.newchatInput}>
-      <div className={style.uploadSection}>
-        <label htmlFor="upload-file" className={style.uploadButton}>
-          <Image className={style.uploadIcon} src={uploadIcon} alt="i" /> PDF
-        </label>
-        <span id="file-name" className={style.fileName}></span>
-        <input id="upload-file" type="file" className={style.addButton} accept="application/pdf" />
+        <h1 className={style.newchatTitle}>Franklin</h1>
       </div>
-      {/* <input className={style.textInput} type="text" value="Poser une question..." /> */}
-      <input className={style.submitButton} type="submit" value=">" />
+
+
+      {/* ZONE D'INPUT */}
+      <div className={style.newchatInput}>
+        {/* UPLOAD UN FICHIER (PDF OU WORD) */}
+        <div className={style.uploadSection}>
+          <label htmlFor="upload-file" className={style.uploadButton}>
+            <Image className={style.uploadIcon} src={uploadIcon} alt="i" /> PDF
+          </label>
+          {/* <span id="file-name" className={style.fileName}></span> */}
+          {/* A AJOUTER COMPOSANT POUR L'AFFICHAGE DES FICHIERS + IMAGE EXTENSION */}
+          <input
+            id="upload-file"
+            type="file"
+            className={style.addButton}
+            accept=".doc, .docx, .pdf"
+          />
+        </div>
+
+        {/* INPUT D'UN MESSAGE AU CHABOT */}
+        <input
+          className={style.textInput}
+          type="text"
+          placeholder="Poser une question..."
+          // value={query}
+          // onChange={test}
+        />
+
+        {/* BOUTON SOUMETTRE LE MESSAGE */}
+        <input className={style.submitButton} type="submit" value=">" />
       </div>
     </div>
   );
